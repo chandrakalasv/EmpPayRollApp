2dde5c6f2b957499be09db46e67b724f521ab03b
chandrakalasv <chandrakalasv1502@gmail.com>
2022-07-12 18:24:43 +0530

    added code to create emp payroll data object

---
+++ js/EmpPayRollData.js
@@ -0,0 +1,68 @@
+class EmployeePayrollData {
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+    
+        this._name = name;
+    }
+
+    get profileImage() {
+        return this._profileImage;
+    }
+
+    set profileImage(profileImage) {
+        this._profileImage = profileImage;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(gender) {
+        this._gender = gender;
+    }
+
+    get department() {
+        return this._department;
+    }
+
+    set department(department) {
+        this._department = department;
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(salary) {
+        this._salary = salary;
+
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+
+    set startDate(startDate) {
+      
+        this._startDate = startDate;
+        
+    }
+
+    get notes() {
+        return this._notes;
+    }
+
+    set notes(notes) {
+        this._notes = notes;
+    }
+
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us", options);
+        return "Id = " + this.id + "\nName = " + this.name + "\nProfile Image = " + this.profileImage + "\nGender = " + this.gender + "\nDepartment = " + this.department + "\nSalary = " + this.salary + "\nStart Date = " + employeeDate + "\nNotes = " + this.notes;
+    }
+}
